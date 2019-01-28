72868351653452f621e979f8c84000ec0e2b2a5f
Zach Gottlieb <zachg27@gmail.com>
2019-01-27 19:52:07 -0500

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'data-app-seed';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log('hello');
+    this.http
+      .get('http://localhost:3000/people')
+      .subscribe(resp => console.log(resp));
+  }
 }
